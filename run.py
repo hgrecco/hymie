543e89a74ec7c7b89eb7bd98798fab5ea1de5688
Hernan <hernan.grecco@gmail.com>
2020-05-29 00:53:22 -0300

    Rewrite run.py to print exceptions better.

--- run.py
+++ run.py
@@ -1,8 +1,19 @@
 import logging
 
-from app import app
+from datastruct.exceptions import MultipleError
 
 from hymie.common import logger
+
+try:
+    from app import app
+except MultipleError as mex:
+    for ex in mex.exceptions:
+        print(
+            "%s %s %s"
+            % (type(ex).__name__, ex.key, ".".join(ex.path).replace(".[", "["))
+        )
+    raise
+
 
 handler = logging.StreamHandler()
 formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
